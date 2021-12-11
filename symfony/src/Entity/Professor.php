623f18ca99363b8792a6f1651c8a4ae4980f2eda
Nicolenco EugeniuN <eugeniu.nicolenco@tekoway.com>
2021-12-11 16:09:25 +0200

    update symfony 6

---
+++ symfony/src/Entity/Professor.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProfessorRepository")
+ */
+class Professor
+{
+    use TimestampableEntity;
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    public int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    public string $name;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Professor
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
